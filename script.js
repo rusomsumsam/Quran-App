93f530d01bb93634411283720f63d5408a6582ab
sumaiya <you@example.com>
2024-10-21 03:32:41 +0600

    Quran App Done

--- script.js
+++ script.js
@@ -1,5 +1,98 @@
 'use strict'
 
+//*Surah Play Starts
+const catchSurahSearchInpt = document.getElementById('surahSearchInpt');
+const catchSurahNumberBtn = document.getElementById('surahNumberBtn');
+const catchSurahOutput = document.getElementById('surahOutput');
+const catchSurahLoader = document.getElementById('surahLoader');
+
+function getInput() {
+    const userValue = catchSurahSearchInpt.value;
+    if (isNaN(userValue)) {
+        alert('You have to enter number value!');
+    } else if (userValue === '') {
+        alert('You have to enter some value!');
+    } else if (userValue <= 114) {
+        resSurahApi(userValue);
+    } else {
+        alert('You have to enter number 1 to 114!');
+    }
+}
+
+function resSurahApi(recUserValue) {
+    catchSurahLoader.style.display = 'block';
+    const inputValue = recUserValue;
+    const apiOne = fetch(`http://api.alquran.cloud/v1/surah/${inputValue}/en.asad`)
+    const apiTwo = fetch(`http://api.alquran.cloud/v1/surah/${inputValue}/ar.alafasy`)
+    const apiThree = fetch(`http://api.alquran.cloud/v1/surah/${inputValue}/bn.bengali`)
+    Promise.all([apiOne, apiTwo, apiThree])
+        .then(responses => Promise.all(responses.map(res => res.json())))
+        .then(data => getSurahApi(data[0], data[1], data[2]))
+}
+
+catchSurahLoader.style.display = 'none';
+
+function getSurahApi(recSurahApiOne, recSurahApiTwo, recSurahApiThree) {
+    const surahApiOne = recSurahApiOne;
+    const surahApiTwo = recSurahApiTwo;
+    const surahApiThree = recSurahApiThree;
+    let ayahCount = 0;
+    
+    function surahPlay() {
+        const surahAyah = surahApiTwo.data.ayahs.length;
+        if (ayahCount < surahAyah) {
+            catchSurahOutput.innerHTML = `
+            <div class="surah_name flex flex-row justify-center gap-2">
+            <p class="ar">${surahApiTwo.data.name}</p>
+            <p class="en">${surahApiOne.data.englishName}</p>
+            </div>
+            <div>
+            <audio id="surahPlayer" src="${surahApiTwo.data.ayahs[ayahCount].audio}" autoplay></audio>
+            </div>
+            <div class="surah_player_dgn flex flex-row justify-center gap-2 items-center">
+            <span><i class="fa-solid fa-backward-fast"></i></span>
+            <button id="surahPlayPause"><i class="fa-solid fa-circle-pause"></i></button>
+            <span><i class="fa-solid fa-forward-fast"></i></span>
+            </div>
+            <div class="surah_tafsir flex flex-col items-center justify-center gap-2">
+            <p class="ar">${surahApiTwo.data.ayahs[ayahCount].text}</p>
+            <p class="en">${surahApiOne.data.ayahs[ayahCount].text}</p>
+            <p class="bn">${surahApiThree.data.ayahs[ayahCount].text}</p>
+            </div>
+            `;
+            const catchSurahPlayer = document.getElementById('surahPlayer');
+            const catchSurahPlayPause = document.getElementById('surahPlayPause');
+            catchSurahPlayer.onended = () => {
+                ayahCount++;
+                surahPlay();
+            }
+            function surahPlayPause() {
+                if (catchSurahPlayer.paused) {
+                    catchSurahPlayer.play();
+                    catchSurahPlayPause.innerHTML = `<i class="fa-solid fa-circle-pause"></i>`;
+                } else {
+                    catchSurahPlayer.pause();
+                    catchSurahPlayPause.innerHTML = `<i class="fa-solid fa-circle-play"></i>`;
+                }
+            }
+            catchSurahPlayPause.onclick = () => {
+                surahPlayPause();
+            }
+        } else {
+            alert('The Surah Has Ended');
+            catchSurahOutput.innerHTML = `<p class="last_message">The End</p>`;
+        }
+    }
+    surahPlay();
+}
+
+catchSurahNumberBtn.addEventListener('click', function () {
+    getInput();
+    catchSurahSearchInpt.value = '';
+})
+
+//*Surah Play Ends
+
 //*Slider Section Starts
 $('.first_slider').owlCarousel({
     loop: true,
@@ -18,7 +111,6 @@ $('.first_slider').owlCarousel({
     }
 })
 //*Slider Section Ends
-
 //*Radio Player Starts
 const catchPlayPause = document.getElementById('playPause');
 const catchRadioOutput = document.getElementById('radioOutput');
@@ -46,3 +138,60 @@ function getRadioApiData(recRadioData) {
         pausePlayBtn();
     }
 }
+//*Radio Player Ends
+//*Live TV Starts
+const catchMakkahTvVideo = document.getElementById('makkahTvVideo');
+const catchMakkahBtn = document.getElementById('makkahBtn');
+const makkahUrl = `https://media2.streambrothers.com:1936/8122/8122/playlist.m3u8`;
+const catchMakkahCrsBtn = document.getElementById('makkahCrsBtn');
+const catchPeaceTvVideo = document.getElementById('peaceTvVideo');
+const catchPeaceTvBtn = document.getElementById('peaceTvBtn');
+const peaceTvUrl = `https://dzkyvlfyge.erbvr.com/PeaceTvBangla/index.m3u8`;
+const catchPeaceCrsBtn = document.getElementById('peaceCrsBtn');
+
+function makkahTvPlay() {
+    if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(makkahUrl);
+        hls.attachMedia(catchMakkahTvVideo);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            video.play();
+        });
+    }
+    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = makkahUrl;
+        video.addEventListener('canplay', function () {
+            video.play();
+        });
+    }
+}
+catchMakkahBtn.addEventListener('click', function () {
+    makkahTvPlay();
+})
+catchMakkahCrsBtn.addEventListener('click', function () {
+    catchMakkahTvVideo.pause();
+})
+
+function peaceTvPlay() {
+    if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(peaceTvUrl);
+        hls.attachMedia(catchPeaceTvVideo);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            video.play();
+        });
+    }
+    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = peaceTvUrl;
+        video.addEventListener('canplay', function () {
+            video.play();
+        });
+    }
+}
+catchPeaceTvBtn.addEventListener('click', function () {
+    peaceTvPlay();
+})
+catchPeaceCrsBtn.addEventListener('click', function () {
+    catchPeaceTvVideo.pause();
+})
+//*Live TV Ends
